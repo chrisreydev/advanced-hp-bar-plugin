60741f96cc0ffc66bafd0041c53bf074f8a1f781
Chris Rey <chrisreycap@gmail.com>
2026-05-17 02:56:15 -0700

    hide all hp bar sprites. NPCs, Player

--- src/main/java/com/advancedhpbar/AdvancedHpBarPlugin.java
+++ src/main/java/com/advancedhpbar/AdvancedHpBarPlugin.java
@@ -1,12 +1,17 @@
 package com.advancedhpbar;
 
 import com.google.inject.Provides;
+import java.io.InputStream;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.game.SpriteManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDependency;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -32,6 +37,12 @@ public class AdvancedHpBarPlugin extends Plugin
     @Inject
     private AdvancedHpBarOverlay overlay;
 
+    @Inject
+    private SpriteManager spriteManager;
+
+    @Inject
+    private ClientThread clientThread;
+
     private long lastTickTime;
 
     @Override
@@ -40,6 +51,7 @@ public class AdvancedHpBarPlugin extends Plugin
         log.info("Advanced HP Bar started!");
         lastTickTime = System.currentTimeMillis();
         overlayManager.add(overlay);
+        clientThread.invoke(this::applyHealthBarOverrides);
     }
 
     @Override
@@ -47,6 +59,61 @@ public class AdvancedHpBarPlugin extends Plugin
     {
         log.info("Advanced HP Bar stopped!");
         overlayManager.remove(overlay);
+        clientThread.invoke(() ->
+        {
+            spriteManager.removeSpriteOverrides(BlankHealthBarOverride.values());
+            client.resetHealthBarCaches();
+        });
+    }
+
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged gameStateChanged)
+    {
+        // Re-apply overrides once the client is past the login screen,
+        // since the sprite cache is not set up until then.
+        if (gameStateChanged.getGameState() == GameState.STARTING)
+        {
+            clientThread.invoke(this::applyHealthBarOverrides);
+        }
+    }
+
+    private boolean applyHealthBarOverrides()
+    {
+        // Sprite cache is not available until the login screen.
+        if (client.getGameState().getState() < GameState.LOGIN_SCREEN.getState())
+        {
+            return false;
+        }
+
+        // --- DEBUG: verify the blank.png resource actually resolves ---
+        String fileName = BlankHealthBarOverride.values()[0].getFileName();
+        try (InputStream in = BlankHealthBarOverride.class.getResourceAsStream(fileName))
+        {
+            if (in != null)
+            {
+                log.info("[AdvancedHpBar] blank.png FOUND ('{}') relative to {}",
+                        fileName, BlankHealthBarOverride.class.getPackage().getName());
+            }
+            else
+            {
+                log.warn("[AdvancedHpBar] blank.png NOT FOUND - getResourceAsStream('{}') returned null. "
+                                + "Expected at src/main/resources/{}/{}",
+                        fileName,
+                        BlankHealthBarOverride.class.getPackage().getName().replace('.', '/'),
+                        fileName);
+            }
+        }
+        catch (Exception ex)
+        {
+            log.warn("[AdvancedHpBar] error while checking blank.png resource", ex);
+        }
+        // --- END DEBUG ---
+
+        spriteManager.addSpriteOverrides(BlankHealthBarOverride.values());
+        client.resetHealthBarCaches();
+        log.info("[AdvancedHpBar] applied {} sprite overrides and reset health bar caches",
+                BlankHealthBarOverride.values().length);
+        return true;
     }
 
     @Subscribe
